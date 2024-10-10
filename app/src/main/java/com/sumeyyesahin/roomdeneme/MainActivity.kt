cadc2aa81a1099d9250532d05fe44c6c70b1e057
Sümeyye Şahin <58478124+sumeyye-sahin@users.noreply.github.com>
2024-10-10 23:29:53 +0300

    Update MainActivity.kt

--- app/src/main/java/com/sumeyyesahin/roomdeneme/MainActivity.kt
+++ app/src/main/java/com/sumeyyesahin/roomdeneme/MainActivity.kt
@@ -14,30 +14,25 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // ViewModel'ı oluşturun
+    
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
-        // Kaydet düğmesine tıklama olayı ekleyin
+      
         binding.button.setOnClickListener {
             val firstName = binding.editTextText.text.toString()
             val lastName = binding.editTextText2.text.toString()
 
             if (firstName.isNotEmpty() && lastName.isNotEmpty()) {
-                // ViewModel aracılığıyla veritabanına kullanıcı ekleyin
+              
                 userViewModel.insertUser(firstName, lastName)
 
-                // Giriş alanlarını temizleyin
+                
                 binding.editTextText.text.clear()
                 binding.editTextText2.text.clear()
             }
         }
 
-        // Kullanıcıları listeleyen bir düğme veya başlangıçta otomatik olarak kullanıcıları alabilirsiniz.
-        // Örneğin:
-        // btnListUsers.setOnClickListener {
-        //     userViewModel.getAllUsers().observe(this, { users ->
-        //         // Kullanıcıları işleyin ve görüntüleyin
-        //     })
-        // }
+        
+    
     }
 }
